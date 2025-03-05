e4d4317b5faaf62d18ec5cddd7acc729cd0be12b
Jay Michalska <hel@lcp.world>
2025-03-05 22:39:25 +0100

    Add lobster

--- src/c/drawing.c
+++ src/c/drawing.c
@@ -1,14 +1,13 @@
 #include "drawing.h"
 
-const int creatures[11][5] = {
+const int creatures[12][5] = {
     {RESOURCE_ID_IMAGE_BEAGLE_1, RESOURCE_ID_IMAGE_BEAGLE_2,
      RESOURCE_ID_IMAGE_BEAGLE_3, RESOURCE_ID_IMAGE_BEAGLE_4,
      RESOURCE_ID_IMAGE_BEAGLE_5},
     {RESOURCE_ID_IMAGE_COW_1, RESOURCE_ID_IMAGE_COW_2, RESOURCE_ID_IMAGE_COW_3,
      RESOURCE_ID_IMAGE_COW_4, RESOURCE_ID_IMAGE_COW_5},
-    {RESOURCE_ID_IMAGE_FOX_1, RESOURCE_ID_IMAGE_FOX_2,
-     RESOURCE_ID_IMAGE_FOX_3, RESOURCE_ID_IMAGE_FOX_4,
-     RESOURCE_ID_IMAGE_FOX_5},
+    {RESOURCE_ID_IMAGE_FOX_1, RESOURCE_ID_IMAGE_FOX_2, RESOURCE_ID_IMAGE_FOX_3,
+     RESOURCE_ID_IMAGE_FOX_4, RESOURCE_ID_IMAGE_FOX_5},
     {RESOURCE_ID_IMAGE_GOAT_1, 0, RESOURCE_ID_IMAGE_GOAT_3,
      RESOURCE_ID_IMAGE_GOAT_4, RESOURCE_ID_IMAGE_GOAT_5},
     {RESOURCE_ID_IMAGE_JERBOA_1, RESOURCE_ID_IMAGE_JERBOA_2,
@@ -17,8 +16,8 @@ const int creatures[11][5] = {
      RESOURCE_ID_IMAGE_PONY_4, RESOURCE_ID_IMAGE_PONY_5},
     {RESOURCE_ID_IMAGE_SHARK_1, RESOURCE_ID_IMAGE_SHARK_2,
      RESOURCE_ID_IMAGE_SHARK_3, 0, RESOURCE_ID_IMAGE_SHARK_5},
-    {RESOURCE_ID_IMAGE_TABBY_1, RESOURCE_ID_IMAGE_TABBY_2,
-     0, 0, RESOURCE_ID_IMAGE_TABBY_5},
+    {RESOURCE_ID_IMAGE_TABBY_1, RESOURCE_ID_IMAGE_TABBY_2, 0, 0,
+     RESOURCE_ID_IMAGE_TABBY_5},
     {RESOURCE_ID_IMAGE_TERRIER_1, RESOURCE_ID_IMAGE_TERRIER_2,
      RESOURCE_ID_IMAGE_TERRIER_3, RESOURCE_ID_IMAGE_TERRIER_4,
      RESOURCE_ID_IMAGE_TERRIER_5},
@@ -26,8 +25,9 @@ const int creatures[11][5] = {
      RESOURCE_ID_IMAGE_BUNNY_3, RESOURCE_ID_IMAGE_BUNNY_4,
      RESOURCE_ID_IMAGE_BUNNY_5},
     {RESOURCE_ID_IMAGE_AXOLOTL_1, 0, RESOURCE_ID_IMAGE_AXOLOTL_3,
-     RESOURCE_ID_IMAGE_AXOLOTL_4, RESOURCE_ID_IMAGE_AXOLOTL_5}
-};
+     RESOURCE_ID_IMAGE_AXOLOTL_4, RESOURCE_ID_IMAGE_AXOLOTL_5},
+    {RESOURCE_ID_IMAGE_LOBSTER_1, RESOURCE_ID_IMAGE_LOBSTER_2, 0, 0,
+     RESOURCE_ID_IMAGE_LOBSTER_5}};
 
 typedef struct {
   GColor color;
@@ -48,12 +48,13 @@ void draw_command_image_in_color(GDrawCommandImage *image, GColor color) {
                              prv_draw_command_list_in_color_cb, &ctx);
 }
 
-void draw_creature_layer(GContext *ctx, int resource, GColor color, GPoint origin) {
-  if (resource == 0) return;
-  
+void draw_creature_layer(GContext *ctx, int resource, GColor color,
+                         GPoint origin) {
+  if (resource == 0)
+    return;
+
   GDrawCommandImage *image = gdraw_command_image_create_with_resource(resource);
   draw_command_image_in_color(image, color);
   gdraw_command_image_draw(ctx, image, origin);
   gdraw_command_image_destroy(image);
 }
-
--- src/c/drawing.h
+++ src/c/drawing.h
@@ -2,7 +2,7 @@
 
 #include <pebble.h>
 
-extern const int creatures[11][5];
+extern const int creatures[12][5];
 
 void draw_command_image_in_color(GDrawCommandImage *image, GColor color);
 void draw_creature_layer(GContext *ctx, int resource, GColor color,
